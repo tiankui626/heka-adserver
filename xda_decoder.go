bf158667dbed0b5dbfe97c1078f8a0076b84fce4
tiankui <tiankui@tiankuideMacBook-Pro.local>
2016-06-01 10:27:53 +0800

    add xda and yda decoder

---
+++ xda_decoder.go
@@ -0,0 +1,71 @@
+package adserver
+
+import (
+	"errors"
+	"fmt"
+	"github.com/mozilla-services/heka/message"
+	"github.com/mozilla-services/heka/pipeline"
+	"regexp"
+	"strings"
+)
+
+type XdaDecoder struct {
+	format string         //x.da 日志正则字符串
+	regexp *regexp.Regexp //x.da 日志正则
+}
+
+func getConfString(config interface{}, key string) (string, error) {
+	var (
+		fieldConf interface{}
+		ok        bool
+	)
+	conf := config.(pipeline.PluginConfig)
+	if fieldConf, ok = conf[key]; !ok {
+		return "", errors.New(fmt.Sprintf("No '%s' setting", key))
+	}
+	value, ok := fieldConf.(string)
+	if ok {
+		return value, nil
+	}
+	return "", nil
+}
+
+func (xd *XdaDecoder) Init(config interface{}) (err error) {
+	format, _ := getConfString(config, "format")
+	if len(format) == 0 {
+		err = errors.New("format config is empty")
+	} else {
+		xd.format = format
+		xd.regexp = regexp.MustCompile(format)
+	}
+	return
+}
+
+func (xd *XdaDecoder) Decode(pack *PipelinePack) (packs []*PipelinePack, err error) {
+	line := pack.Message.GetPayload()
+	if !xd.regexp.Match([]byte(line)) {
+		fmt.Printf("regexp error:%s\n", err.Error())
+		return
+	}
+	parsedLine := strings.Replace(line, " ", "&", -1)
+	values, err := url.ParseQuery(parsedLine)
+	if err != nil {
+		fmt.Printf("parse line error:%s\n", err.Error())
+		return
+	}
+	for k, vs := range values {
+		field := message.NewFieldInit(k, message.Field_STRING, "")
+		for _, v := range vs {
+			field.AddValue(v)
+		}
+		pack.Message.AddField(field)
+	}
+
+	return []*PipelinePack{pack}, nil
+}
+
+func init() {
+	RegisterPlugin("XdaDecoder", func() interface{} {
+		return new(XdaDecoder)
+	})
+}
---
+++ yda_decoder.go
@@ -0,0 +1,74 @@
+package adserver
+
+import (
+	"errors"
+	"fmt"
+	"github.com/mozilla-services/heka/message"
+	"github.com/mozilla-services/heka/pipeline"
+	"regexp"
+	"strings"
+)
+
+type XdaDecoder struct {
+	format   string         //y.da 日志正则字符串
+	regexp   *regexp.Regexp //y.da 日志正则
+	queryKey string         //y.da query字段
+}
+
+func getConfString(config interface{}, key string) (string, error) {
+	var (
+		fieldConf interface{}
+		ok        bool
+	)
+	conf := config.(pipeline.PluginConfig)
+	if fieldConf, ok = conf[key]; !ok {
+		return "", errors.New(fmt.Sprintf("No '%s' setting", key))
+	}
+	value, ok := fieldConf.(string)
+	if ok {
+		return value, nil
+	}
+	return "", nil
+}
+
+func (xd *XdaDecoder) Init(config interface{}) (err error) {
+	format, _ := getConfString(config, "format")
+	queryKey, _ := getConfString(config, "query")
+	if len(format) == 0 {
+		err = errors.New("format config is empty")
+	} else {
+		xd.format = format
+		re := regexp.MustCompile(`\\\$([a-z_]+)(\\?(.))`).ReplaceAllString(
+			regexp.QuoteMeta(format+" "), "(?P<$1>[^$3]*)$2")
+		xd.regexp = regexp.MustCompile(fmt.Sprintf("^%v$", strings.Trim(re, " ")))
+	}
+	xd.queryKey = queryKey
+	return
+}
+
+func (xd *XdaDecoder) Decode(pack *PipelinePack) (packs []*PipelinePack, err error) {
+	line := pack.Message.GetPayload()
+
+	fields := xd.regexp.FindStringSubmatch(line)
+	if fields == nil {
+		err = fmt.Errorf("access log line '%v' does not match given format '%v'", line, re)
+		return
+	}
+
+	for i, name := range re.SubexpNames() {
+		if i == 0 {
+			continue
+		}
+		field := message.NewFieldInit(name, message.Field_STRING, "")
+		field.AddValue(fields[i])
+		pack.Message.AddField(field)
+	}
+
+	return []*PipelinePack{pack}, nil
+}
+
+func init() {
+	RegisterPlugin("XdaDecoder", func() interface{} {
+		return new(XdaDecoder)
+	})
+}
