f70e6aad6510eb25ac3f89c92820870e714baf6c
tiankui <tiankui@tiankuideMacBook-Pro.local>
2016-06-01 15:50:50 +0800

    xda debug config

--- xda_decoder.go
+++ xda_decoder.go
@@ -13,6 +13,7 @@ import (
 type XdaDecoder struct {
 	format string         //x.da 日志正则字符串
 	regexp *regexp.Regexp //x.da 日志正则
+	debug  bool           //debug
 }
 
 func getConfString(config interface{}, key string) (string, error) {
@@ -33,17 +34,23 @@ func getConfString(config interface{}, key string) (string, error) {
 
 func (xd *XdaDecoder) Init(config interface{}) (err error) {
 	format, _ := getConfString(config, "format")
+	debug, _ := getConfString(config, "debug")
+	fmt.Printf("xdadecoder init, format:%s, debug:%s\n", format, debug)
 	if len(format) == 0 {
 		err = errors.New("format config is empty")
 	} else {
 		xd.format = format
 		xd.regexp = regexp.MustCompile(format)
 	}
+	xd.debug = (debug == "1")
 	return
 }
 
 func (xd *XdaDecoder) Decode(pack *pipeline.PipelinePack) (packs []*pipeline.PipelinePack, err error) {
 	line := pack.Message.GetPayload()
+	if xd.debug {
+		fmt.Printf("decode line:%s\n", line)
+	}
 	if !xd.regexp.Match([]byte(line)) {
 		fmt.Printf("regexp error:%s\n", err.Error())
 		return
